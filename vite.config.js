f171bd32ffa09f9d9e15653936475571750fc785
Hitesh Vekariya <hiteshvekariya012@gmail.com>
2026-06-30 13:53:48 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://hitu02-creater.github.io/Budget_Web-Application/" /**/,
+  base: "https://hitu02-creater.github.io/" /**/,
   build: {
     chunkSizeWarningLimit: 2000, // 1000 KB
   },
